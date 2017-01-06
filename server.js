86bf1358a40cd4d0ce29d82ec7fac2b57712c678
Ritwik Ruia <ritwikruia@gmail.com>
2017-01-06 14:01:27 -0800

    adds answers up to visitors

--- server.js
+++ server.js
@@ -5,20 +5,54 @@ var app = express();
 // GET '/'
 app.get('/', function(req, res) {
   // render a link to '/counter'
+  var html = "<a href=\"/counter\">counter</a>" //can be written as: var html = "<a href='/counter'>counter</a>" to avoid using \ to escape from normal flow
+  res.send(html);
 });
 
 // GET '/counter?vote=up'
 // GET '/counter?vote=down'
   // increment a counter up or down
 
+app.get('/counter', function(req, res){
+  var counter = 0; //counter getting to 0 on every page load
+  if (req.query.vote === 'up') {      //?vote=down || ?vote=up
+    counter++;
+  } else if (req.query.vote === 'down') {
+    counter--;
+  }
+  res.send('counter is' + counter);
+
+
+})
+
 // Respond to
 // GET '/greet/dave'
 // GET '/greet/susan'
   // render <h1>Hello [name]!</h1>
   // and a link to '/visitors'
+  // https://expressjs.com/en/guide/routing.html#route-parameters
+var names = [];
+app.get('/greet/:name', function(req, res){ // colon looks for pattern as explained in url above
+    var name = req.params.name
+    var title = "<h1> Hello " + name + "!</h1>";
+    var link = "<a href='/visitors'>Visitors</a>";
+    names.push(name);
+    res.send(title + link);
+
+
+})
 
 // GET '/visitors'
   // render a list of all the names from '/greet/:name'
+app.get('/visitors', function(req, res){
+  var list = "<ul>"
+  for(var i = 0; i <names.length; i++) {
+    var list = list + "<li>" + names[i] + "</li>";
+  }
+  list = list + "</ul>"
+  res.send(list);
+})
+
 
 // GET '/visitors.json'
   // send a list of all the names from '/greet/:name' as json
